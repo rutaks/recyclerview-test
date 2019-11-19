8acdf06e9afc46a8db9ea0821193f21e178c80c4
rutaks <samrutakayile@awesomity.rw>
2019-11-19 21:57:34 +0200

    Setup Custom Recycler View Adapter

--- app/src/main/java/com/example/recyclerviewtest/ItemAdapter.java
+++ app/src/main/java/com/example/recyclerviewtest/ItemAdapter.java
@@ -1,19 +1,23 @@
 package com.example.recyclerviewtest;
 
-import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder>{
+    private ArrayList<Item> items;
     public static class ItemViewHolder extends RecyclerView.ViewHolder {
         public ImageView imageView;
         public TextView firstTextView;
         public TextView secondTextView;
+
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.imageView);
@@ -22,19 +26,28 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
         }
     }
 
+    public ItemAdapter(ArrayList<Item> items) {
+        this.items = items;
+    }
+
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        ItemViewHolder viewHolder = new ItemViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
-
+        Item currentItem = items.get(position);
+        holder.imageView.setImageResource(currentItem.getImageResource());
+        holder.firstTextView.setText(currentItem.getFirstText());
+        holder.secondTextView.setText(currentItem.getSecondText());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 }
