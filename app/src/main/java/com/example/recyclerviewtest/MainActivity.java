bcdcd85e3cba673440ffc2875463619a66c531a6
rutaks <samrutakayile@awesomity.rw>
2019-11-20 22:01:57 +0200

    Added Insert & Remove functionalities

--- app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -5,25 +5,66 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Item> items;
+
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    Button buttonInsert;
+    Button buttonRemove;
+    EditText editTextInsert;
+    EditText editTextRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Item> items = new ArrayList<>();
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
 
-        items.add(new Item(R.drawable.ic_android, "Line 1", "Line 2"));
-        items.add(new Item(R.drawable.ic_apps, "Line 1", "Line 2"));
-        items.add(new Item(R.drawable.ic_folder, "Line 1", "Line 2"));
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
 
+        setList();
+        setRecyclerView();
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+    }
+
+    public void insertItem(int position){
+        items.add(position, new Item(R.drawable.ic_apps, "New Item At Position " + position, "Line 2"));
+        adapter.notifyDataSetChanged();
+    }
+
+    public void removeItem(int position){
+        items.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void setRecyclerView(){
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
@@ -31,5 +72,12 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+    }
+
+    public void setList(){
+        items = new ArrayList<>();
+        items.add(new Item(R.drawable.ic_android, "Line 1", "Line 2"));
+        items.add(new Item(R.drawable.ic_apps, "Line 1", "Line 2"));
+        items.add(new Item(R.drawable.ic_folder, "Line 1", "Line 2"));
     }
 }
